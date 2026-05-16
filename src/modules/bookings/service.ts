37faa5e86808a67f140f72d90d01b9766c693dfe
ripabak <faruqirifa2@gmail.com>
2026-05-16 21:03:36 +0700

    fix: remove past date validation for appointment scheduling & fix timezone

--- src/modules/bookings/service.ts
+++ src/modules/bookings/service.ts
@@ -252,13 +252,6 @@ export abstract class BookingService {
 			)
 		}
 
-		if (scheduledAt.getTime() <= Date.now()) {
-			throw new AppError(
-				'Appointment scheduledAt must be in the future',
-				'BAD_REQUEST'
-			)
-		}
-
 		await BookingService.validateOpenHours(organizationId, scheduledAt)
 
 		return scheduledAt
